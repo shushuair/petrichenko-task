53da1c9c35fbbf8b8f5ea185c42892144464c957
Ilya <shushulkov1989@gmail.com>
2023-01-14 19:09:32 +0000

    new task to js file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+};
+
+let a = prompt("Один из последних просмотренных фильмов?", "");
+let b = prompt("На сколько оцените его?", "");
+let c = prompt("Один из последних просмотренных фильмов?", "");
+let d = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
