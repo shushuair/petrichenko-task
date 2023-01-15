c877f7bf8c27ec36532d47062be5dfdbc6976624
Ilya <shushulkov1989@gmail.com>
2023-01-15 19:01:40 +0000

    change task4.js

--- js/task4.js
+++ js/task4.js
@@ -20,3 +20,45 @@
 
 // - функция должна продолжать работать, даже если изменяется количество магазинов, высота, бюджет или подставляется вообще другой объект.
 
+const shoppingMallData = {
+   shops: [
+       {
+           width: 10,
+           length: 5
+       },
+       {
+           width: 15,
+           length: 7
+       },
+       {
+           width: 20,
+           length: 5
+       },
+       {
+           width: 8,
+           length: 10
+       }
+   ],
+   height: 5,
+   moneyPer1m3: 30,
+   budget: 50000
+}
+
+function isBudgetEnough(data) {
+   let square = 0;
+   let volume = 0;
+
+   data.shops.forEach(shop => {
+       square += shop.width * shop.length;
+   });
+
+   volume = data.height * square;
+
+   if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+       return 'Бюджета достаточно';
+   } else {
+       return 'Бюджета недостаточно';
+   }
+}
+
+isBudgetEnough(shoppingMallData);
